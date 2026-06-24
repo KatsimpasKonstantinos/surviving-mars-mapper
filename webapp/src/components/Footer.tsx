9242572e2db2db6f376fddc88e1384a49db2ce29
Konsti <134274002+KatsimpasKonstantinos@users.noreply.github.com>
2026-06-24 18:35:10 +0200

    Update Footer.tsx

--- webapp/src/components/Footer.tsx
+++ webapp/src/components/Footer.tsx
@@ -4,9 +4,9 @@ import './Footer.css';
 function Footer() {
     return (
         <footer className="Footer">
-            <p><a href="https://konsti.zip/impressum" target="_blank" rel="noopener noreferrer">Impressum</a></p>
+            <p><a href="https://konsti.zip/impressum">Impressum</a></p>
             <p>|</p>
-            <p><a href="https://konsti.zip/datenschutz" target="_blank" rel="noopener noreferrer">Datenschutz</a></p>
+            <p><a href="https://konsti.zip/datenschutz">Datenschutz</a></p>
         </footer>
     )
 }
