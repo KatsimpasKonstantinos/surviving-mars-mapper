d486fa4ff1a29f652e3d89869f0d2181cb2d1c71
Konsti <134274002+KatsimpasKonstantinos@users.noreply.github.com>
2026-07-07 23:15:46 +0200

    fix cf build error

--- webapp/src/sites/Finder.tsx
+++ webapp/src/sites/Finder.tsx
@@ -29,6 +29,8 @@ const parseNumber = (val: string | number | undefined) => {
 
 function Finder({ coordString, setCoordString, mapData }: FinderProps) {
 
+  console.log(coordString);
+
   const rowData = useMemo<FinderRowData[]>(() => {
     return Object.entries(mapData).map(([coord, data]) => ({
       Coordinate: coord,
